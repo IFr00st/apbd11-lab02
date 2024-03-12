c331aa393fc0b0408116fa48148738295bdcb2ad
s27885 <s27885@pjwstk.edu.pl>
2024-03-12 15:14:23 +0100

    Add GetAverage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Mod 1");
 Console.WriteLine("Mod 2");
 Console.WriteLine("Mod 3");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    foreach(int x in numbers)
+    {
+        sum += x;
+    }
+
+    return sum / numbers.Length;
+}
